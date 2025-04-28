893343cc3c49eb9a97329229ad954eb66c471226
Manthan-Mishra <manthan9481147615@gmail.com>
2025-04-29 02:23:54 +0530

    functional component changed to class based

--- Ep-01/src/components/Demo.js
+++ Ep-01/src/components/Demo.js
@@ -2,25 +2,39 @@ import React, { useState } from "react";
 import "./demo.css";
 import DemoClass from "./DemoClass";
 
-const Demo = ({ contact, name, location }) => {
-  const [count0, setCount0] = useState(0);
-  const [count1, setCount1] = useState(1);
-  // console.log("contact ", contact);
-  // console.log("name ", name);
-  // console.log("location ", location);
+class Demo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count0: 0,
+      count1: 1,
+    };
+  }
 
-  return (
-    <>
-      <div className="user-card">
-        <h1>Count: {count0}</h1>
-        <h5>Count: {count1}</h5>
-        <h2>Name: {name}</h2>
-        <h3>Location: {location}</h3>
-        <h4>Contact: {contact}</h4>
-      </div>
-      <DemoClass name={name} location={location} contact={contact} />
-    </>
-  );
-};
+  handleInc = () => {
+    this.setState({
+      count0: this.state.count0 + 1,
+      count1: this.state.count1 + 1,
+    });
+  };
+
+  render() {
+    const { count0, count1 } = this.state;
+    const { name, location, contact } = this.props;
+    return (
+      <>
+        <div className="user-card">
+          <h1>Count: {count0}</h1>
+          <h5>Count: {count1}</h5>
+          <button onClick={this.handleInc}>Click to increase</button>
+          <h2>Name: {name}</h2>
+          <h3>Location: {location}</h3>
+          <h4>Contact: {contact}</h4>
+        </div>
+        <DemoClass name={name} location={location} contact={contact} />
+      </>
+    );
+  }
+}
 
 export default Demo;
